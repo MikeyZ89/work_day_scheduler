717fe64ace1244ca6f7a2fbda64f9ecc6a192120
Michael Zlotkowski <michael.zlotkowski@gmail.com>
2020-05-13 16:56:13 -0700

    commit edits

--- app.js
+++ app.js
@@ -14,18 +14,25 @@ $(".time-block").each(function(){
     console.log(hour, currentHour);
 });
 
-$("#9a").children("textarea").val(localStorage.getItem("9a"));
-$("#10a").children("textarea").val(localStorage.getItem("10a"));
-$("#11a").children("textarea").val(localStorage.getItem("11a"));
-$("#12a").children("textarea").val(localStorage.getItem("12a"));
-$("#13a").children("textarea").val(localStorage.getItem("13a"));
-$("#14a").children("textarea").val(localStorage.getItem("14a"));
-$("#15a").children("textarea").val(localStorage.getItem("15a"));
-$("#16a").children("textarea").val(localStorage.getItem("16a"));
-$("#17a").children("textarea").val(localStorage.getItem("17a"));
+$("#9a").val(localStorage.getItem("9a"));
+$("#10a").val(localStorage.getItem("10a"));
+$("#11a").val(localStorage.getItem("11a"));
+$("#12a").val(localStorage.getItem("12a"));
+$("#13a").val(localStorage.getItem("13a"));
+$("#14a").val(localStorage.getItem("14a"));
+$("#15a").val(localStorage.getItem("15a"));
+$("#16a").val(localStorage.getItem("16a"));
+$("#17a").val(localStorage.getItem("17a"));
 
 $('button').on('click', function (){
     let time = $(this).parent().attr('id');
     let text = $(this).siblings('textarea').val();
     localStorage.setItem(time, text);
 });
+
+// //Save function, detects a click anywhere in the container, then checks if the click is on an element with class saveBtn, before getting the specific row from an assigned ID on the button.
+// $(".container").on("click", function(e){
+//     if(e.target.classList.contains("saveBtn")){ 
+//         planner[e.target.id] = $(e.target).prev()[0].value; //This seems needlessly complex.
+//         localStorage.setItem("planner", JSON.stringify(planner));
+//     }
